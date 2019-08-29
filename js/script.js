879bab1d207a95626a48d988e4c634cb7466b674
Elena Ordina <post032@yandex.ru>
2019-08-29 17:13:13 +0300

    page

---
+++ js/script.js
@@ -0,0 +1,17 @@
+(function() {
+    "use strict";
+    var toggles = document.querySelectorAll('.menu__open');
+    var open = document.querySelector('#nav');
+    for (var i = toggles.length - 1; i >= 0; i--) {
+      var toggle = toggles[i];
+      toggleHandler(toggle);
+    };
+
+    function toggleHandler(toggle) {
+      toggle.addEventListener("click", function(e) {
+        e.preventDefault();
+        open.classList.toggle('menu__hide');
+        (this.classList.contains("is-active") === true) ? this.classList.remove("is-active"): this.classList.add("is-active");
+      });
+    }
+  })();
